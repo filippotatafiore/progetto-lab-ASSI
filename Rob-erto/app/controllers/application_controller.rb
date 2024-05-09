66bb5e6853299f604f59325bf6181a602ffedbfc
filippotatafiore <filippotatafiore.01@gmail.com>
2024-05-09 19:41:26 +0200

    Modifica bottone per le impostazioni, aggiunta modifiche per tema day-night (non funziona)

--- Rob-erto/app/controllers/application_controller.rb
+++ Rob-erto/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def index
+        # ------------------------------------------------tema app
+        if params[:theme].present?
+            @theme = params[:theme] 
+        else
+            @theme = 'light'
+        end
+    end
+
 end
