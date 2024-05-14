daba1cb5bc6ba0695e1dfe8c8ae109804b5cc45c
DennisPS4 <alecann0n3@gmail.com>
2024-05-14 15:47:38 +0200

    rigenerato la pagina help, il controllore e le route con rails generate

--- Rob-erto/config/routes.rb
+++ Rob-erto/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
   get 'index' => 'home#index'
   root 'home#index'
-  get 'help' => 'home#help'
+  get 'help' => 'help#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
