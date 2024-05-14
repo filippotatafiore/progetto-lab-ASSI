1ff77f17f738d0d512d20e98ce05214913e320bf
DennisPS4 <alecann0n3@gmail.com>
2024-05-14 13:00:03 +0200

    bottone help reindirizza alla pagina help, aggiunto importmap, layout base della tabella help

--- Rob-erto/config/routes.rb
+++ Rob-erto/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  #get 'home/index'
+  get 'index' => 'home#index'
   root 'home#index'
+  get 'help' => 'home#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
