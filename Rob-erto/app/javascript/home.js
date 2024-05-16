c316df080aa2f5bb743b43d35e000e6c1713b233
filippotatafiore <filippotatafiore.01@gmail.com>
2024-05-16 13:43:42 +0200

    modifiche chatflow

--- Rob-erto/app/javascript/home.js
+++
@@ -1,9 +0,0 @@
-
-var send_button = document.getElementById('send_button');
-var form = document.getElementById('user_input_form');
-
-send_button.addEventListener('click', function () {
-    form.submit();
-    //console.log("form submitted");
-});
-
