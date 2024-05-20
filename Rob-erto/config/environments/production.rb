1efe494cbb06ed6f37ed95c67b77a651cee3079d
Armidon1 <nole.1940213@studenti.uniroma1.it>
2024-05-20 12:39:40 +0200

    added login and register view with devise

--- Rob-erto/config/environments/production.rb
+++ Rob-erto/config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Allow connections to local server. required for devise. wi will change this later with a real host and port (maybe)
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- Rob-erto/config/routes.rb
+++ Rob-erto/config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
   post '/send_msg', to: 'home#send_msg'     # per invocare send_msg di HomeController dalla view
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users,:controllers => {registrations: 'registrations'}
+  #change the route from /users/login to /login route
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
 end
