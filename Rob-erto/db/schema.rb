61bbfb242b134a1043035c6c40aed0fd14bc8849
DennisPS4 <alecann0n3@gmail.com>
2024-05-23 18:52:33 +0200

    creato modello chat

--- Rob-erto/db/schema.rb
+++ Rob-erto/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_23_110727) do
+ActiveRecord::Schema.define(version: 2024_05_23_165059) do
+
+  create_table "chats", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "nome"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
