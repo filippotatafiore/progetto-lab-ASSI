0ce6d5ac5852d2a579a1654e0c38bc9f96af709c
Armidon1 <nole.1940213@studenti.uniroma1.it>
2024-06-14 14:53:48 +0200

    seed ora popola le tabelle user e friendship per test

--- Rob-erto/db/seeds.rb
+++ Rob-erto/db/seeds.rb
@@ -6,13 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Crea l'utente corrente
-# current_user = User.find_by(email: 'simo.nole@gmail.com')
-
+# --------------------------------------------------------------------------------------------Popola Users
 # # Crea altri utenti
-# user2 = User.create!(email: 'user2@example.com', password: 'password', name: 'User2')
-# user3 = User.create!(email: 'user3@example.com', password: 'password', name: 'User3')
+# users = 10.times.map do
+#   User.create!(
+#     email: Faker::Internet.unique.email,
+#     password: 'password',
+#     name: Faker::Name.name,
+#     provider: 'provider',
+#     uid: SecureRandom.uuid,
+#     sign_in_count: rand(1..10),
+#     current_sign_in_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+#     last_sign_in_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+#     current_sign_in_ip: Faker::Internet.ip_v4_address,
+#     last_sign_in_ip: Faker::Internet.ip_v4_address,
+#     profile_image: rand(1..10),
+#     nickname: Faker::Internet.username
+#   )
+# end
+
+# --------------------------------------------------------------------------------------------Popola Friendships
+# Crea l'utente corrente
+# current_user = User.find_by(email: 'simo.nole12@gmail.com')
+
+# # Prendi tutti gli utenti
+# users = User.all
+
+# # Prendi tre utenti casuali
+# random_user1 = users.sample
+# random_user2 = users.sample
+# random_user3 = users.sample
 
 # # Crea amicizie
-# Friendship.create!(user_id: current_user.id, friend_id: user2.id, status: 0) # pending
-# Friendship.create!(user_id: current_user.id, friend_id: user3.id, status: 1) # accepted
+# Friendship.create!(user_id: random_user1.id, friend_id: current_user.id, status: 0) # pending by random_user1
+# Friendship.create!(user_id: current_user.id, friend_id: random_user2.id, status: 0) # pending by current_user to random_user2
+# Friendship.create!(user_id: current_user.id, friend_id: random_user3.id, status: 1) # accepted random_user3
+# Friendship.create!(user_id: random_user3.id, friend_id: current_user.id, status: 1) # accepted duplicated for database
+#--------------------------------------------------------------------------------------------
