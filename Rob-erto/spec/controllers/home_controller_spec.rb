2584181a392dc04813f86d2f667c6e83abeb0720
filippotatafiore <filippotatafiore.01@gmail.com>
2024-06-21 13:31:03 +0200

    fixato test invio messaggi send_msg

--- Rob-erto/spec/controllers/home_controller_spec.rb
+++ Rob-erto/spec/controllers/home_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
-# Capybara.default_driver = :selenium
+Capybara.default_driver = :selenium
 
-# RSpec.describe "Chat", type: :feature do
-#   it "sends a message to the AI and receives a response" do
-#     visit root_path
-#     fill_in "user_input", with: "Hello!"
-#     click_button "send_button"
+RSpec.describe "Chat", type: :feature do
+  it "sends a message to the AI and receives a response" do
+    visit root_path
+    fill_in "user_input", with: "Hello!"
+    click_button "send_button"
 
-#     Capybara.default_max_wait_time = 5  # Aumenta il tempo massimo di attesa
-#     # Aspetta che i chatbox appaiano sulla pagina (dopo esecuzione di send_msg)
-#     expect(page).to have_css(".chatbox.outgoing:last-child p", text: "Hello!", visible: true)
-#     expect(page).to have_css(".chatbox.incoming:last-child p", text: /./, visible: true)
+    Capybara.default_max_wait_time = 5  # Aumenta il tempo massimo di attesa
+    # Aspetta che i chatbox appaiano sulla pagina (dopo esecuzione di send_msg)
+    expect(page).to have_css(".chatbox.outgoing:last-child p", text: "Hello!", visible: true)
+    expect(page).to have_css(".chatbox.incoming:last-child p", text: /./, visible: true)
 
-#   end
-# end
+  end
+end
 
 RSpec.describe HomeController, type: :controller do
   describe "POST #send_msg" do
@@ -27,14 +27,14 @@ RSpec.describe HomeController, type: :controller do
     it "sends a message to the AI" do
       post :send_msg, params: { user_input: "Hello!" }
 
-      expect(response).to be_successful
-      expect(session[:messages].last["content"]).to eq("Hello!")
+      expect(response).to redirect_to(action: :index)
+      expect(session[:messages].last).to eq({"role": "user", "content": "Hello!"})
     end
 
     it "receives a response from the AI" do
       post :send_msg, params: { user_input: "Hello!" }
 
-      expect(response).to be_successful
+      expect(response).to redirect_to(action: :index)
       expect(session[:response]).not_to be_nil
     end
   end
