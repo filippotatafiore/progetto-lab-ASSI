d139d2f66dd93d3ed0d91b7ec82eeaad702c32d9
Armidon1 <nole.1940213@studenti.uniroma1.it>
2024-06-26 13:08:11 +0200

    implemntato tutte le view di devise

--- Rob-erto/app/controllers/application_controller.rb
+++ Rob-erto/app/controllers/application_controller.rb
@@ -35,4 +35,20 @@ class ApplicationController < ActionController::Base
     def resource_name
         devise_mapping.name
     end
+
+    def after_sign_in_path_for(resource)
+        # Imposta qui la tua variabile di sessione
+        session[:user_id] = current_user.id
+        super
+    end
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
